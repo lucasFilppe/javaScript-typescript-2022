d1bb326b452603bb05e881da9c2f211e6fefa38a
lucasFilppe <lucasfilipe2323@icloud.com>
2023-02-14 23:55:30 -0300

    validando input

--- 6-programação-orientada-objetos/aula3/index.js
+++ 6-programação-orientada-objetos/aula3/index.js
@@ -12,8 +12,35 @@ class ValidaFormulario{
 
     handleSubmit(e) {
         e.preventDefault();
-        console.log("formulario nao enviado")
-    }
-}
+        //console.log("formulario nao enviado")
 
+        const camposValidos = this.camposSaoValidos(); 
+    }
+
+    camposSaoValidos(){
+        let valido = true;
+
+        for(let errorText of this.formulario.querySelectorAll('.error-text')){
+                errorText.remove()
+        }
+
+        for(let campo of this.formulario.querySelectorAll('.validar')){
+            const label = campo.previousElementSibling.innerHTML
+
+            //console.log(campo)//ver o que estamos pegando
+            if(!campo.value){
+                this.criaErro(campo, `Campo ${label} nao pode estar em branco`)
+                valido = false;
+            }
+        }
+    }
+
+    criaErro(campo, msg){
+        const div = document.createElement('div')
+        div.innerHTML = msg
+        div.classList.add('error-text')
+        campo.insertAdjacentElement('afterend', div);
+    }
+
+}
 const valida = new ValidaFormulario()
