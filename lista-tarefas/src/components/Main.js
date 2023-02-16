992c9b687be09a84ae8bb377e0e0d20a576cd75f
lucasFilppe <lucasfilipe2323@icloud.com>
2023-02-15 21:59:45 -0300

    criando primeiro estado

--- lista-tarefas/src/components/Main.js
+++ lista-tarefas/src/components/Main.js
@@ -1,15 +1,30 @@
 import { render } from "@testing-library/react";
 import React, { Component } from "react";
-
+import "./Main.css"
 class Main extends Component {
-  constructor(props) {
-    super(props)
+
+    state = {
+      novaTarefa: '',
+    };
+
+    handleChange = (e) =>{
+    this.setState({
+      novaTarefa: e.target.value
+    });
   }
 
   render() {
+
+    const {novaTarefa} = this.state//
+
     return (
-      <div>
-        <h1>Meu componente estado</h1>
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+        <form action="#">
+          <input  onChange={this.handleChange} type="text"/> {/*Onchange chama uma função quando o usuario alterar o campo do input*/}
+          <button type="submit">Enviar</button>
+          <p>{novaTarefa}</p>
+        </form>
       </div>
     );
   }
