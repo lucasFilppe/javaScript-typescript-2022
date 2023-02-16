9bbe54772aec812c9861d9738c6eac831aa27e4b
lucasFilppe <lucasfilipe2323@icloud.com>
2023-02-16 12:59:19 -0300

    criando funcionalidade das tarefas

--- lista-tarefas/src/components/Main.js
+++ lista-tarefas/src/components/Main.js
@@ -1,10 +1,21 @@
 import { render } from "@testing-library/react";
 import React, { Component } from "react";
-import "./Main.css"
+import "./Main.css";
+
+//form
+import {FaPlus} from "react-icons/fa"
+
+//tarefas
+ import {FaEdit, FaWindowClose} from "react-icons/fa"
 class Main extends Component {
 
     state = {
       novaTarefa: '',
+      tarefas: [
+        'fazer café',
+        'beber água',
+        'estudar',
+      ],
     };
 
     handleChange = (e) =>{
@@ -15,16 +26,35 @@ class Main extends Component {
 
   render() {
 
-    const {novaTarefa} = this.state//
+    const {novaTarefa, tarefas} = this.state//
 
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
-        <form action="#">
-          <input  onChange={this.handleChange} type="text"/> {/*Onchange chama uma função quando o usuario alterar o campo do input*/}
-          <button type="submit">Enviar</button>
-          <p>{novaTarefa}</p>
+        <form action="#" className="form">
+          <input  
+            onChange={this.handleChange} 
+            type="text"
+            value={novaTarefa}
+          /> {/*Onchange chama uma função quando o usuario alterar o campo do input*/}
+          <button type="submit">
+            <FaPlus/>
+          </button>
         </form>
+
+        <ul className="tarefas">
+
+          {tarefas.map((tarefa) =>(
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit"/>
+                <FaWindowClose className="delete"/>
+              </div>
+            </li>
+          ))}
+
+        </ul>
       </div>
     );
   }
