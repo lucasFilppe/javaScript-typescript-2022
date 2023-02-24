e4546a75946a27dc06dc9080e03793fa64a4c581
lucasFilppe <lucasfilipe2323@icloud.com>
2023-02-23 22:26:00 -0300

    funcionalidade de adicionar tarefa concluida

--- lista-tarefas/src/components/Main.js
+++ lista-tarefas/src/components/Main.js
@@ -11,12 +11,23 @@ class Main extends Component {
 
     state = {
       novaTarefa: '',
-      tarefas: [
-        'fazer café',
-        'beber água',
-        'estudar',
-      ],
+      tarefas: [],
     };
+
+    handleSubmit = (e) =>{
+      e.preventDefault();
+      
+      const {tarefas} = this.state;
+      let {novaTarefa} = this.state;
+      novaTarefa = novaTarefa.trim();
+
+      if(tarefas.indexOf(novaTarefa) !== -1) return;//indexOf retorna a posição de um valor especificado do array
+
+      const novasTarefas = [...tarefas];
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+      });
+    }
 
     handleChange = (e) =>{
     this.setState({
@@ -31,12 +42,12 @@ class Main extends Component {
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form"> {/*o evento onSubimit ocorre quando um formulario é enviado */}
           <input  
-            onChange={this.handleChange} 
+            onChange={this.handleChange} /*Onchange chama uma função quando o usuario alterar o campo do input*/
             type="text"
             value={novaTarefa}
-          /> {/*Onchange chama uma função quando o usuario alterar o campo do input*/}
+          /> 
           <button type="submit">
             <FaPlus/>
           </button>
